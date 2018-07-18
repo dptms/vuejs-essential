32e82269e1f127da1ff4ccf3b25bd0bf63f8e1e0
dp~ <457509824@qq.com>
2018-07-18 14:19:20 +0800

    register

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -11,28 +11,28 @@
                         <div class="form-group">
                             <label class="control-label">用户名</label>
                             <input v-validator:input.required="{ regex: /^[a-zA-Z]+\w*\s?\w*$/, error: '用户名要求以字母开头的单词字符' }"
-                                   type="text" class="form-control" placeholder="请填写用户名">
+                                   type="text" class="form-control" placeholder="请填写用户名" v-model.trim="username">
                         </div>
                         <div class="form-group">
                             <label class="control-label">密码</label>
                             <input id="password"
                                    v-validator.required="{ regex: /^\w{6,16}$/, error: '密码要求 6 ~ 16 个单词字符' }"
-                                   type="password" class="form-control" placeholder="请填写密码">
+                                   type="password" class="form-control" placeholder="请填写密码" v-model.trim="password">
                         </div>
                         <div class="form-group">
                             <label class="control-label">确认密码</label>
                             <input v-validator.required="{ target: '#password' }" type="password" class="form-control"
-                                   placeholder="请填写确认密码">
+                                   placeholder="请填写确认密码" v-model.trim="cpassword">
                         </div>
                         <div class="form-group">
                             <label class="control-label">图片验证码</label>
                             <input v-validator.required="{ title: '图片验证码' }" type="text" class="form-control"
-                                   placeholder="请填写验证码">
+                                   placeholder="请填写验证码" v-model.trim="captcha">
                         </div>
-                        <div class="thumbnail" title="点击图片重新获取验证码" @click="getCaptcha()">
+                        <div class="thumbnail" title="点击图片重新获取验证码" @click="getCaptcha">
                             <div class="captcha vcenter" v-html="captchaTpl"></div>
                         </div>
-                        <button type="submit" class="btn btn-lg btn-success btn-block">
+                        <button type="submit" class="btn btn-lg btn-success btn-block" @click="register">
                             <i class="fa fa-btn fa-sign-in"></i> 注册
                         </button>
                     </div>
@@ -44,12 +44,17 @@
 
 <script>
     import createCaptcha from '@/utils/createCaptcha'
+    import ls from '@/utils/localStorage'
 
     export default {
         name: "Register",
         data() {
             return {
-                captchaTpl: '' // 验证码模板
+                captchaTpl: '', // 验证码模板
+                username: '', // 用户名
+                password: '', // 密码
+                cpassword: '', // 确认密码
+                captcha: '', // 验证码
             }
         },
         created() {
@@ -60,6 +65,42 @@
                 const {tpl, captcha} = createCaptcha()
                 this.captchaTpl = tpl
                 this.localCaptcha = captcha
+            },
+            register(e) {
+                this.$nextTick(() => {
+                    const target = e.target.type === 'submit' ? e.target : e.target.parentElement
+
+                    if (target.canSubmit) {
+                        this.submit()
+                    }
+                })
+            },
+            submit() {
+                if (this.captcha.toUpperCase() !== this.localCaptcha) {
+                    alert('验证码不正确')
+                    this.getCaptcha()
+                } else {
+                    const user = {
+                        name: this.username,
+                        password: this.password,
+                        avatar: `https://api.adorable.io/avatars/200/${this.username}.png`
+                    }
+                    const localUser = ls.getItem('user')
+
+                    if (localUser) {
+                        if (localUser.name === user.name) {
+                            alert('用户名已存在')
+                        } else {
+                            this.login(user)
+                        }
+                    } else {
+                        this.login(user)
+                    }
+                }
+            },
+            login(user) {
+                ls.setItem('user', user)
+                alert('注册成功')
             }
         }
     }
